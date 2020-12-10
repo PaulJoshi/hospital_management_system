f729e6573ef35e5908481cca956c10937c9e7520
Parvathi Ambareesh <neon9rj@gmail.com>
2020-12-10 18:27:04 +0530

    Added prescribe_medicine method

--- core/employees.py
+++ core/employees.py
@@ -34,10 +34,17 @@ class Doctor :
 
         return appointments
     
-    def prescribe_medicine(self, patient_name) :
+    def prescribe_medicine(self, patient_name,prescription) :
 
         ''' Write medicine prescription name into most recent appointment list in database '''
-        
+        with open ("database/appointments.pkl","rb") as file:
+            all_appointments=pickle.load(file) 
+        for i in reversed(range(len(all_appointments))) :
+            if all_appointments[i][0].lower()==self.name.lower() and all_appointments[i][1].lower()==patient_name.lower() :
+                all_appointments[i].append(prescription)
+
+        with open ("database/appointments.pkl","wb") as file:
+            pickle.dump(all_appointments,file)
 
 
 class Nurse :
